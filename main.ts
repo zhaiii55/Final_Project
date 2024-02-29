0e9f7e5f33919d4878f3f17dd4ca5e12eff7e4ac
zhaiii55 <156235611+zhaiii55@users.noreply.github.com>
2024-02-29 11:01:23 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,13 @@ namespace SpriteKind {
     export const Healer = SpriteKind.create()
     export const CLEANERPICKER = SpriteKind.create()
     export const MESS = SpriteKind.create()
+    export const RAT_ATTACK = SpriteKind.create()
+    export const FOOD_FOR_RAT = SpriteKind.create()
+    export const THE_BIG_DEAL = SpriteKind.create()
 }
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     MOP = sprites.create(assets.image`myImage0`, SpriteKind.CLEANERPICKER)
-    MOP.setPosition(Chef_1.x + 2, Chef_1.y + 0)
+    MOP.setPosition(Chef_1.x + 5, Chef_1.y + 0)
     animation.runImageAnimation(
     MOP,
     [img`
@@ -80,24 +83,113 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
     500,
     true
     )
+    pause(1500)
+    sprites.destroy(MOP)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.FOOD_FOR_RAT, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.confetti, 500)
+    numBAKEDTREATS = numBAKEDTREATS + 1
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile15`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level1`)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    MOP_ATTACK = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . c c c c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . . . c 6 9 c . . . . . . 
+        . . . . c c c 6 9 c c c . . . . 
+        . . . . c 6 9 9 9 9 9 c . . . . 
+        . . . . c 5 4 5 4 5 4 c . . . . 
+        . . . c c 5 4 5 4 5 4 c . . . . 
+        . . . c 5 4 5 5 4 5 5 4 c . . . 
+        . . . c c c c c c c c c c . . . 
+        `, SpriteKind.RAT_ATTACK)
+    MOP_ATTACK.setPosition(Chef_1.x + 5, Chef_1.y + 0)
+    animation.runImageAnimation(
+    MOP_ATTACK,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        9 . 9 9 . . . . . . . . . . . . 
+        9 9 9 9 9 9 9 9 9 9 . . . 9 5 . 
+        . . . . . 9 9 9 9 9 9 9 9 9 5 5 
+        . . . . . . . . 9 9 9 9 9 5 5 5 
+        . . . . . . . . . . . 9 9 5 5 5 
+        . . . . . . . . . . . 9 9 5 5 5 
+        . . . . . . . . . . 9 9 5 5 5 . 
+        . . . . . . . . . . 9 9 5 5 5 . 
+        . . . . . . . . . . . 5 5 5 5 . 
+        . . . . . . . . . . . 5 5 5 . . 
+        . . . . . . . . . . . . . 5 . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . 9 . . . . . . . . . . . . . 
+        . . 9 9 9 . . . . . . . . . . . 
+        . . . 9 9 . . . . . . 1 . . . . 
+        . . . . 9 9 . . . . . 1 . . . . 
+        . . . . 9 9 9 . . . . 1 . . 1 . 
+        . . . . . 9 9 9 9 . . . . . 1 . 
+        . . . . . . 9 9 9 9 . . . . . . 
+        . 1 1 1 . . 9 9 9 9 9 . . . . . 
+        . . . 1 1 . . 9 9 9 9 . 9 9 . . 
+        . . . . . . . . 9 9 9 9 9 9 9 . 
+        . . . . . . . . . 9 9 9 9 5 5 5 
+        . 1 1 1 . 9 9 9 9 9 9 5 5 5 5 5 
+        . . . . . 9 9 9 9 5 5 5 5 5 5 5 
+        . . . . . . 9 5 5 5 5 5 5 5 5 5 
+        . . . . . . . 5 5 5 5 5 5 5 . . 
+        `],
+    500,
+    true
+    )
+    pause(1000)
+    sprites.destroy(MOP_ATTACK)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.RAT_ATTACK, function (sprite, otherSprite) {
+    sprites.destroy(sprite, effects.spray, 500)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Healer, function (sprite, otherSprite) {
     info.changeLifeBy(1)
     sprites.destroy(otherSprite)
 })
 sprites.onOverlap(SpriteKind.MESS, SpriteKind.CLEANERPICKER, function (sprite, otherSprite) {
-	
+    sprites.destroy(sprite)
+    pause(1000)
+    sprites.destroy(otherSprite)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
-    pause(500)
     otherSprite.follow(sprite, 55)
+    pause(500)
 })
+let sprinkle_cake: Sprite = null
+let list: Image[] = []
+let MOP_ATTACK: Sprite = null
 let MOP: Sprite = null
+let numBAKEDTREATS = 0
 let Mess: Sprite = null
+let STRAWBERRY_CAKE: Sprite = null
+let magical_cheese: Sprite = null
+let PINK_Healing_Cake: Sprite = null
 let Healing_Cake: Sprite = null
 let Rat__Enemy: Sprite = null
 let Chef_1: Sprite = null
+game.splash("The bakery is closing")
+game.splash("Theres no janitor to clean")
+game.splash("Looks like its your new job.")
+game.splash("Make sure you collect all the food.")
+game.splash("Be careful with the rats...")
 info.setLife(5)
 Chef_1 = sprites.create(assets.image`chef`, SpriteKind.Player)
 controller.moveSprite(Chef_1)
@@ -270,11 +362,66 @@ for (let index = 0; index < 15; index++) {
     Rat__Enemy = sprites.create(assets.image`RATS`, SpriteKind.Enemy)
     tiles.placeOnRandomTile(Rat__Enemy, assets.tile`myTile8`)
 }
-for (let index = 0; index < 15; index++) {
+for (let index = 0; index < 10; index++) {
     Healing_Cake = sprites.create(assets.image`Yummy Treat`, SpriteKind.Healer)
     tiles.placeOnRandomTile(Healing_Cake, assets.tile`myTile8`)
 }
-for (let index = 0; index < 20; index++) {
+for (let index = 0; index < 5; index++) {
+    PINK_Healing_Cake = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . c . . c . . . . . . 
+        . . . . . c 7 c c 7 c . . . . . 
+        . . . . . c c 2 d c c . . . . . 
+        . . . . c 3 3 2 2 3 3 c . . . . 
+        . . . c 3 3 3 3 3 3 3 3 c . . . 
+        . . . c 3 3 3 3 e 3 e 3 c . . . 
+        . . . c 3 e 3 e 4 e 4 3 c . . . 
+        . . c c e 4 e 4 4 4 4 3 c c . . 
+        . . c d 4 4 4 4 4 4 4 e d c . . 
+        . . c d d d d d d d d d d c . . 
+        . . c c c c c c c c c c c c . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.FOOD_FOR_RAT)
+    tiles.placeOnRandomTile(PINK_Healing_Cake, assets.tile`myTile8`)
+    magical_cheese = sprites.create(assets.image`Cheese fothe ratking`, SpriteKind.FOOD_FOR_RAT)
+    tiles.placeOnRandomTile(magical_cheese, assets.tile`myTile8`)
+    STRAWBERRY_CAKE = sprites.create(assets.image`cakeforratking`, SpriteKind.FOOD_FOR_RAT)
+    tiles.placeOnRandomTile(STRAWBERRY_CAKE, assets.tile`myTile8`)
+}
+for (let index = 0; index < 10; index++) {
     Mess = sprites.create(assets.image`WHAT A MESS`, SpriteKind.MESS)
     tiles.placeOnRandomTile(Mess, assets.tile`myTile8`)
 }
+let ENEMY_KING_RAT = sprites.create(assets.image`THE EVIL RAT`, SpriteKind.THE_BIG_DEAL)
+info.setScore(0)
+let Goal = 10
+tiles.placeOnTile(ENEMY_KING_RAT, tiles.getTileLocation(5, 26))
+numBAKEDTREATS = 0
+forever(function () {
+    if (info.score() == Goal) {
+        game.showLongText("You cleaned the whole place up! Good Job!", DialogLayout.Bottom)
+    }
+    if (numBAKEDTREATS == 20) {
+        game.showLongText("You've collected enough baked treats!", DialogLayout.Bottom)
+    }
+    list = [assets.image`Cheese fothe ratking`, assets.image`cakeforratking`]
+    if (info.life() < 4) {
+        for (let index = 0; index < 5; index++) {
+            sprinkle_cake = sprites.create(assets.image`treat`, SpriteKind.FOOD_FOR_RAT)
+            tiles.placeOnRandomTile(sprinkle_cake, assets.tile`myTile8`)
+        }
+        list.push(assets.image`treat`)
+        info.changeLifeBy(1)
+    }
+    if (numBAKEDTREATS == 10 && info.score() == Goal && Chef_1.overlapsWith(ENEMY_KING_RAT)) {
+        tiles.placeOnTile(Chef_1, tiles.getTileLocation(6, 26))
+        game.showLongText("RAHHHHHHHHHHHH!!!!", DialogLayout.Bottom)
+        scene.cameraShake(10, 1000)
+        game.showLongText("You have killed my people!", DialogLayout.Bottom)
+        game.showLongText("NOW YOU SHALL PAY!", DialogLayout.Bottom)
+        tiles.setCurrentTilemap(tilemap`level`)
+    }
+})
